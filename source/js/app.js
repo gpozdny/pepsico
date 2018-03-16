1629b34217def293fd08d8687640cdd937eb0fa6
Gennady Yudin <soulmatesneverdie92@gmail.com>
2018-03-16 03:34:55 +0300

    circles without color change

--- source/js/app.js
+++ source/js/app.js
@@ -1,48 +1,74 @@
 "use strict";
 
-$( document ).ready(function() {
-    
+$(document).ready(function () {
+
     // filter dropdown
-    $('.list__filter').click(function(){
+    $('.list__filter').click(function () {
 
         let dropdown = $('.filter__dropdown');
         let filter = $('.list__filter');
 
         dropdown.toggleClass("filter__dropdown--active");
         filter.toggleClass("list__filter--active");
-        
+
     });
 
-    // modals
+    // popups
 
     let briefing = $('#briefing'),
+        presentation = $('#presentation'),
         overlay = $('.profile__modals-overlay'),
-        modal = $('.profile__modals'),
+        modal_briefing = $('.profile__modals'),
+        modal_presentation = $('.profile__modals-presentation'),
         cross = $('.profile__modal-cross'),
         wrapper = $('.wrapper');
 
-    briefing.click(function(){
+    // Бриф
+    briefing.click(function () {
 
         overlay.css("display", "block");
-        modal.css("display", "block");
+        modal_briefing.css("display", "block");
         wrapper.addClass('blur');
 
     });
 
-    overlay.click(function(){
+
+
+    // Презентация
+    presentation.click(function () {
+
+        overlay.css("display", "block");
+        modal_presentation.css("display", "block");
+        wrapper.addClass('blur');
+
+    });
+    // overlay & cross
+
+    overlay.click(function () {
 
         $(this).css("display", "none");
-        modal.css("display", "none");
+        modal_briefing.css("display", "none");
+        modal_presentation.css("display", "none");
         wrapper.removeClass('blur');
 
     });
 
-    cross.click(function(){
+    cross.click(function () {
 
         overlay.css("display", "none");
-        modal.css("display", "none");
+        modal_briefing.css("display", "none");
+        modal_presentation.css("display", "none");
         wrapper.removeClass('blur');
 
     });
+    // jQuery Knob
 
+    $(".dial").knob({
+        'min': 0,
+        'max': 10,
+        'rotation': "anticlockwise",
+        'change' : function (v) {
+            console.log(v);
+         }
+    });
 });
