27478ce754b168a0f6caa18e1a323ca1ca89b3a1
Gennady Yudin <soulmatesneverdie92@gmail.com>
2018-03-21 22:50:09 +0300

    popups

--- source/js/app.js
+++ source/js/app.js
@@ -17,9 +17,12 @@ $(document).ready(function () {
 
     let $briefing = $('#briefing'),
         $presentation = $('#presentation'),
+        $finals = $('#finals'),
+        $this = $(this),
         $overlay = $('.profile__modals-overlay'),
         $modal_briefing = $('.profile__modals'),
         $modal_presentation = $('.profile__modals-presentation'),
+        $modal_fail = $('.profile__modals-fail'),
         $cross = $('.profile__modal-cross'),
         $wrapper = $('.wrapper');
 
@@ -61,6 +64,26 @@ $(document).ready(function () {
         $wrapper.removeClass('blur');
 
     });
+
+
+    // items' backgrounds
+
+    if ($briefing.is('.profile__item--active')) {
+        $briefing.addClass('profile__item-icon--idea-white')
+    }
+
+    if ($finals.is('.profile__item--active')) {
+        $finals.addClass('profile__item-icon--finals-active')
+    } else if ($finals.is('.profile__item--success')) {
+        $finals.addClass('profile__item-icon--finals-success')
+    }
+
+    if ($presentation.is('.profile__item--active')) {
+        $presentation.addClass('profile__item-icon--presentation-active')
+    } else if ($presentation.is('.profile__item--success')) {
+        $presentation.addClass('profile__item-icon--presentation-success')
+    }
+
     // jQuery Knob
 
     // $(".dial").knob({
@@ -100,10 +123,14 @@ $(document).ready(function () {
 
 let $inputName = $('.profile__attachments-input'),
     $this = $(this),
+    $docxPresentation = $('#docxPresentation'),
+    $pptx = $('#pptx'),
     $docx = $('#docx'),
     $image = $('#image'),
     $labelDocx = $('#labelDocx'),
     $labelImage = $('#labelImage'),
+    $labelPresentation = $('#labelPresentation'),
+    $labelDesc = $('#labelDesc'),
     $attchBtn = $('.profile__attachments-btn');
 
 
@@ -111,12 +138,41 @@ function changeDocx() {
     $docx.removeClass('hidden');
     $docx.addClass('show');
     $labelDocx.addClass('hidden');
-}
+};
 
 function changeImage() {
     $image.removeClass('hidden');
     $image.addClass('show');
     $labelImage.addClass('hidden');
     $attchBtn.addClass('btn--active');
-}
+};
 
+function changeDesc() {
+    $docxPresentation.removeClass('hidden');
+    $docxPresentation.addClass('show');
+    $labelDesc.addClass('hidden');
+};
+
+function changePresentation() {
+    $pptx.removeClass('hidden');
+    $pptx.addClass('show');
+    $labelPresentation.addClass('hidden');
+    $attchBtn.addClass('btn--active');
+};
+
+function previewFile() {
+
+    let preview = document.querySelector('#image');
+    let file = document.querySelector('#imgInput').files[0];
+    let reader = new FileReader();
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+    }
+}
